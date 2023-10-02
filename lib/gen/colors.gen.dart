2744386a36468991c09ea9c9d88f0bbedf1cd79d
tenpei-sano <kikichan.fps@gmail.com>
2023-10-02 18:38:49 +0900

    lintやasset、colorの初期設定

---
+++ lib/gen/colors.gen.dart
@@ -0,0 +1,27 @@
+/// GENERATED CODE - DO NOT MODIFY BY HAND
+/// *****************************************************
+///  FlutterGen
+/// *****************************************************
+
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// ignore_for_file: directives_ordering,unnecessary_import,implicit_dynamic_list_literal,deprecated_member_use
+
+import 'package:flutter/painting.dart';
+import 'package:flutter/material.dart';
+
+class AppColor {
+  AppColor._();
+
+  /// Color: #6D8B74
+  static const Color accentColor = Color(0xFF6D8B74);
+
+  /// Color: #FAF9F4
+  static const Color firstColor = Color(0xFFFAF9F4);
+
+  /// Color: #D0C9C0
+  static const Color secondColor = Color(0xFFD0C9C0);
+
+  /// Color: #4d4d4d
+  static const Color textColor = Color(0xFF4D4D4D);
+}
