00b00d002ebbbc5c5f5683e3ad457bd5b111c4ec
tenpei-sano <kikichan.fps@gmail.com>
2023-10-03 14:36:58 +0900

    UserDataモデル作成

---
+++ lib/domain/entity/user_data.dart
@@ -0,0 +1,14 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'user_data.freezed.dart';
+part 'user_data.g.dart';
+
+@freezed
+abstract class UserData with _$UserData {
+  const factory UserData({
+    required String id,
+    required String email,
+    required String name,
+  }) = _UserData;
+  factory UserData.fromJson(Map<String, dynamic> json) => _$UserDataFromJson(json);
+}
