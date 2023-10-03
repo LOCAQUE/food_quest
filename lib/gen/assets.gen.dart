f3be99417d00f97c653f6d03582a5410f7a9cc84
tenpei-sano <kikichan.fps@gmail.com>
2023-10-03 13:25:56 +0900

    lintエラー解消

--- lib/gen/assets.gen.dart
+++ lib/gen/assets.gen.dart
@@ -9,8 +9,20 @@
 
 import 'package:flutter/widgets.dart';
 
+class $AssetsImagesGen {
+  const $AssetsImagesGen();
+
+  /// File path: assets/images/test.png
+  AssetGenImage get test => const AssetGenImage('assets/images/test.png');
+
+  /// List of all assets
+  List<AssetGenImage> get values => [test];
+}
+
 class Assets {
   Assets._();
+
+  static const $AssetsImagesGen images = $AssetsImagesGen();
 }
 
 class AssetGenImage {
