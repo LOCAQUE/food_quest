ffa5088892a47a1dd2b20fb7c5f06a892cde7225
tenpei-sano <kikichan.fps@gmail.com>
2023-10-03 12:51:49 +0900

    テキストフィールドの共通コンポーネント作成

---
+++ lib/presentation/component/custom_text_field.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class CustomTextField extends StatelessWidget {
+  const CustomTextField({
+  required this.controller,
+  required this.hintText,
+  this.obscureText = false,
+  super.key,
+});
+
+final TextEditingController controller;
+final String hintText;
+final bool obscureText;
+
+@override
+Widget build(BuildContext context) {
+  return TextFormField(
+    controller: controller,
+    obscureText: obscureText,
+    decoration: InputDecoration(
+      filled: true,
+      fillColor: const Color(0xffe3dfdc),
+      contentPadding:
+      const EdgeInsets.symmetric(vertical: 24, horizontal: 24),
+      border: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(40),
+        borderSide: BorderSide.none,
+      ),
+      hintText: hintText,
+    ),
+  );
+}
+}
