0323befd0242a7dc0841659ffdf7ead5dbc2c5a8
maple-fm <fuma.deguchi@gmail.com>
2023-10-04 23:36:47 +0900

    feat: ログイン画面の追加

---
+++ lib/presentation/screen/auth/sign_in_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class SignInScreen extends StatelessWidget {
+  const SignInScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'ログイン画面',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
