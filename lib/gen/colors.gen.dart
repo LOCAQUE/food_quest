4b1b555d6849cfbd6084aba690b96f544d558dd6
maple-fm <fuma.deguchi@gmail.com>
2023-10-05 00:28:33 +0900

    feat: ボタンカラーの追加

--- lib/gen/colors.gen.dart
+++ lib/gen/colors.gen.dart
@@ -7,8 +7,8 @@
 // ignore_for_file: type=lint
 // ignore_for_file: directives_ordering,unnecessary_import,implicit_dynamic_list_literal,deprecated_member_use
 
-import 'package:flutter/material.dart';
 import 'package:flutter/painting.dart';
+import 'package:flutter/material.dart';
 
 class AppColor {
   AppColor._();
@@ -16,8 +16,14 @@ class AppColor {
   /// Color: #6D8B74
   static const Color accentColor = Color(0xFF6D8B74);
 
+  /// Color: #8E8E93
+  static const Color disabledColor = Color(0xFF8E8E93);
+
   /// Color: #FAF9F4
   static const Color firstColor = Color(0xFFFAF9F4);
+
+  /// Color: #EE7D50
+  static const Color primaryColor = Color(0xFFEE7D50);
 
   /// Color: #D0C9C0
   static const Color secondColor = Color(0xFFD0C9C0);
@@ -25,9 +31,6 @@ class AppColor {
   /// Color: #4d4d4d
   static const Color textColor = Color(0xFF4D4D4D);
 
-  /// Color: #EE7D50
-  static const Color primaryColor = Color(0xFFEE7D50);
-
-  /// Color: #8E8E93
-  static const Color disabledColor = Color(0xFF8E8E93);
+  /// Color: #FFFFFF
+  static const Color white = Color(0xFFFFFFFF);
 }
