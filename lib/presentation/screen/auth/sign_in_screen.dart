65eda39c6f11058ee71051382b59fe649af42818
maple-fm <fuma.deguchi@gmail.com>
2023-10-05 21:29:52 +0900

    feat: 戻るボタンの実装

--- lib/presentation/screen/auth/sign_in_screen.dart
+++ lib/presentation/screen/auth/sign_in_screen.dart
@@ -5,8 +5,20 @@ class SignInScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        leading: IconButton(
+          icon: const Icon(
+            Icons.arrow_back_ios,
+            color: Colors.black,
+          ),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+      body: const Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
