1ad11675a1928ec28dcfb30fdc8f474255b6c9a9
kaka0913 <kabuyuu0913@gmail.com>
2023-10-05 10:29:38 +0900

    ペットページにゲージを追加

--- lib/presentation/screen/pet/pet_screen.dart
+++ lib/presentation/screen/pet/pet_screen.dart
@@ -1,20 +1,84 @@
 import 'package:flutter/material.dart';
 
-class PetScreen extends StatelessWidget {
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class PetScreen extends ConsumerWidget {
   const PetScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'ペット育成画面',
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Column(
+        children: [
+          const SizedBox(height: 50),
+          Padding(
+            padding: const EdgeInsets.all(10),
+            child: Row(
+              children: [
+                const CircleAvatar(
+                  radius: 30,
+                  backgroundColor: Color(0xFFEE7D50),
+                  child: Text(
+                    '45',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 30,
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: SizedBox(
+                    height: 15,
+                    child: LinearProgressIndicator(
+                      value: 1,
+                      backgroundColor: Colors.grey[300],
+                      valueColor: const AlwaysStoppedAnimation(
+                        Color(0xFFE4AB94),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
             ),
-          ],
-        ),
+          ),
+          const Padding(
+            padding: EdgeInsets.only(left: 200),
+            child: Text(
+              'EXP',
+              style: TextStyle(
+                fontSize: 16,
+                color: Colors.black,
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 40),
+            child: Row(
+              children: [
+                const Icon(Icons.favorite, color: Colors.red, size: 40),
+                Expanded(
+                  child: SizedBox(
+                    height: 15,
+                    child: LinearProgressIndicator(
+                      value: 0.5,
+                      backgroundColor: Colors.grey[300],
+                      valueColor: const AlwaysStoppedAnimation(
+                        Color(0xFFE4AB94),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          IconButton(
+            icon: const Icon(Icons.arrow_back),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+          ),
+        ],
       ),
     );
   }
