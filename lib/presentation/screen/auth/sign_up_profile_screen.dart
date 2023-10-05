6dacde03a680878a7fba9ad105eac124ccb1574a
maple-fm <fuma.deguchi@gmail.com>
2023-10-06 03:20:42 +0900

    feat: プロフィール入力画面のUI作成

--- lib/presentation/screen/auth/sign_up_profile_screen.dart
+++ lib/presentation/screen/auth/sign_up_profile_screen.dart
@@ -1,20 +1,89 @@
 import 'package:flutter/material.dart';
 
+import 'package:gap/gap.dart';
+
+import 'package:food_quest/domain/entity/list.dart';
+import 'package:food_quest/presentation/component/button.dart';
+import 'package:food_quest/presentation/component/custom_date_picker.dart';
+import 'package:food_quest/presentation/component/custom_picker.dart';
+import 'package:food_quest/presentation/component/custom_text_field.dart';
+
 class SignUpProfile extends StatelessWidget {
-  const SignUpProfile({super.key});
+  SignUpProfile({super.key});
+
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController userNameController = TextEditingController();
+  final TextEditingController genderController = TextEditingController();
+  final TextEditingController birthdayController = TextEditingController();
+  final TextEditingController addressController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'プロフィール入力画面',
+    return Scaffold(
+      backgroundColor: const Color(0xFFFFF5E0),
+      body: ListView(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(40),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Gap(10),
+                const Text(
+                  'プロフィール入力',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const Gap(10),
+                Padding(
+                  padding: const EdgeInsets.only(top: 18),
+                  child: CustomTextField(
+                    title: 'ニックネーム',
+                    controller: nameController,
+                    hintText: 'ニックネームを入力してください',
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 18),
+                  child: CustomTextField(
+                    title: 'ユーザーネーム',
+                    controller: userNameController,
+                    hintText: '@username',
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 18),
+                  child: CustomPicker(
+                    options: genderList,
+                    title: '性別',
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 18),
+                  child: CustomDatePicker(
+                    title: '生年月日',
+                    controller: birthdayController,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 18),
+                  child: CustomPicker(
+                    options: prefectures,
+                    title: '居住地',
+                  ),
+                ),
+                Gap(80),
+                CustomButton(
+                  text: 'ペット選択へ',
+                  onPressed: () {},
+                  variant: ButtonVariant.primary,
+                ),
+              ],
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
