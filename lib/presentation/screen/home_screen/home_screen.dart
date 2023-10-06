a0fd9ae607fa291393dc856a02ce3a6dd2399560
maple-fm <fuma.deguchi@gmail.com>
2023-10-07 04:11:50 +0900

    feat: 上タブの骨組みの構築

--- lib/presentation/screen/home_screen/home_screen.dart
+++ lib/presentation/screen/home_screen/home_screen.dart
@@ -11,21 +11,41 @@ class HomeScreen extends HookConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
-      body: Column(
-        children: [
-          const Gap(40),
-          const Gap(20), // 追加したボタンとの間隔を開けるためのスペース
-          ElevatedButton(
-            onPressed: () {
-              Navigator.of(context).push(
-                MaterialPageRoute<QuestionScreen>(
-                  builder: (context) => const QuestionScreen(),
-                ),
-              );
-            },
-            child: const Text('新しいページへ'),
-          ),
-        ],
+      body: DefaultTabController(
+        length: 2,
+        child: Column(
+          children: [
+            const SizedBox(height: 50), // 画面上部の余白
+            const TabBar(
+              tabs: [
+                Tab(text: 'Tab1'),
+                Tab(text: 'Tab2'),
+              ],
+            ),
+            Expanded(
+              child: TabBarView(
+                children: [
+                  Column(
+                    children: [
+                      const Gap(20), // 追加したボタンとの間隔を開けるためのスペース
+                      ElevatedButton(
+                        onPressed: () {
+                          Navigator.of(context).push(
+                            MaterialPageRoute<QuestionScreen>(
+                              builder: (context) => const QuestionScreen(),
+                            ),
+                          );
+                        },
+                        child: const Text('新しいページへ'),
+                      ),
+                    ],
+                  ),
+                  const Center(child: Text('Tab2')),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
