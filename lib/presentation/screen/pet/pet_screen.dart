0300b0563e2fa2ad895fb2a92758b85779e0aca6
maple-fm <fuma.deguchi@gmail.com>
2023-10-07 01:35:47 +0900

    update: 丸とゲージを重ねた

--- lib/presentation/screen/pet/pet_screen.dart
+++ lib/presentation/screen/pet/pet_screen.dart
@@ -1,14 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:food_quest/gen/colors.gen.dart';
 
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-class PetScreen extends ConsumerWidget {
+import 'package:food_quest/gen/colors.gen.dart';
+
+class PetScreen extends HookConsumerWidget {
   const PetScreen({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    const level = 1;
+    const maxEXP = 150;
+    const exp = 20;
+
     return Scaffold(
       body: Container(
         decoration: const BoxDecoration(
@@ -21,40 +25,46 @@ class PetScreen extends ConsumerWidget {
           children: [
             const SizedBox(height: 50),
             Padding(
-              padding: const EdgeInsets.all(20),
-              child: Row(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Stack(
+                alignment: Alignment.centerLeft,
                 children: [
+                  Row(
+                    children: [
+                      const SizedBox(width: 30), // CircleAvatarの幅と一致するスペース
+                      Expanded(
+                        child: SizedBox(
+                          height: 15, // EXPのゲージを太くします
+                          child: LinearProgressIndicator(
+                            borderRadius: BorderRadius.circular(50),
+                            value: exp / maxEXP, // 割合
+                            backgroundColor: Colors.grey[300],
+                            valueColor: const AlwaysStoppedAnimation(
+                              AppColor.primaryColor,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
                   const CircleAvatar(
                     radius: 30,
                     backgroundColor: AppColor.primaryColor,
                     child: Text(
-                      '1',
+                      '$level',
                       style: TextStyle(
                         color: Colors.white,
                         fontSize: 30,
                       ),
                     ),
                   ),
-                  Expanded(
-                    child: SizedBox(
-                      height: 15, // EXPのゲージを太くします
-                      child: LinearProgressIndicator(
-                        borderRadius: BorderRadius.circular(50),
-                        value: 0,
-                        backgroundColor: Colors.grey[300],
-                        valueColor: const AlwaysStoppedAnimation(
-                          Color(0xFFE4AB94),
-                        ),
-                      ),
-                    ),
-                  ),
                 ],
               ),
             ),
-            const Padding(
-              padding: EdgeInsets.only(left: 200),
-              child: Text(
-                '経験値:0/150',
+            Container(
+              alignment: Alignment.topRight,
+              child: const Text(
+                '経験値: $exp/$maxEXP',
                 style: TextStyle(
                   fontSize: 16,
                   color: Colors.white,
@@ -63,23 +73,31 @@ class PetScreen extends ConsumerWidget {
             ),
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 20),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              child: Stack(
+                alignment: Alignment.centerLeft,
                 children: [
-                  const SizedBox(width: 40),
-                  const Icon(Icons.favorite, color: Colors.red, size: 40),
-                  Expanded(
-                    child: SizedBox(
-                      height: 10, // ハートのゲージを太くします
-                      child: LinearProgressIndicator(
-                        value: 0.5,
-                        borderRadius: BorderRadius.circular(50),
-                        backgroundColor: Colors.grey[300],
-                        valueColor: const AlwaysStoppedAnimation(
-                          Color(0xFFE4AB94),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      const SizedBox(width: 40),
+                      Expanded(
+                        child: SizedBox(
+                          height: 10, // ハートのゲージを太くします
+                          child: LinearProgressIndicator(
+                            value: 0.5,
+                            borderRadius: BorderRadius.circular(50),
+                            backgroundColor: Colors.grey[300],
+                            valueColor: const AlwaysStoppedAnimation(
+                              AppColor.primaryColor,
+                            ),
+                          ),
                         ),
                       ),
-                    ),
+                    ],
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.only(left: 20),
+                    child: Icon(Icons.favorite, color: Colors.red, size: 40),
                   ),
                 ],
               ),
