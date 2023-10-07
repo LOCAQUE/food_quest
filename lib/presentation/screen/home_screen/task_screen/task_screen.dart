4b2350e25b2ea7ed7f9bcc8f8e2c2960ddada14c
maple-fm <fuma.deguchi@gmail.com>
2023-10-08 02:05:01 +0900

    feat: タスク画面のnotifierの作成

--- lib/presentation/screen/home_screen/task_screen/task_screen.dart
+++ lib/presentation/screen/home_screen/task_screen/task_screen.dart
@@ -4,17 +4,27 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import 'package:food_quest/domain/notifier/question_task_notifier.dart';
 import 'package:food_quest/presentation/screen/home_screen/component/task_component.dart';
+import 'package:food_quest/presentation/screen/home_screen/task_screen/task_screen_notifier.dart';
 
 class TaskScreen extends HookConsumerWidget {
   const TaskScreen({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final isLoading = ref.watch(
+      taskScreenNotifierProvider.select((state) => state.isLoading),
+    );
     final taskList = ref.watch(
           questionTaskNotifierProvider.select((state) => state.taskList),
         ) ??
         [];
     print('taskList: $taskList');
+
+    if (isLoading) {
+      return const Scaffold(
+        body: Center(child: CircularProgressIndicator()),
+      );
+    }
 
     if (taskList.isEmpty) {
       return const SizedBox.shrink();
