e30a7d0515d6007c2490e37b2e67f6d04f7218ab
maple-fm <fuma.deguchi@gmail.com>
2023-10-07 13:13:04 +0900

    update: リストで表示できるように修正

--- lib/presentation/screen/home_screen/component/task_component.dart
+++ lib/presentation/screen/home_screen/component/task_component.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
 import 'package:food_quest/gen/colors.gen.dart';
 
-class TaskComponent extends StatelessWidget {
+class TaskComponent extends HookConsumerWidget {
   const TaskComponent({
     required this.text,
     required this.now,
@@ -15,51 +17,48 @@ class TaskComponent extends StatelessWidget {
   final int achievement;
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Card(
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-        child: SizedBox(
-          width: 310,
-          height: 95,
-          child: Padding(
-            padding: EdgeInsets.symmetric(horizontal: 10),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    Text(text),
-                  ],
-                ),
-                Stack(
-                  alignment: Alignment.center,
-                  children: [
-                    Expanded(
-                      child: SizedBox(
-                        height: 16, // EXPのゲージを太くします
-                        child: LinearProgressIndicator(
-                          borderRadius: BorderRadius.circular(50),
-                          value: now / achievement,
-                          backgroundColor: Colors.grey[300],
-                          valueColor: const AlwaysStoppedAnimation(
-                            AppColor.primaryColor,
-                          ),
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Card(
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+      child: SizedBox(
+        width: 310,
+        height: 95,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Row(
+                children: [
+                  Text(text),
+                ],
+              ),
+              Stack(
+                alignment: Alignment.center,
+                children: [
+                  Expanded(
+                    child: SizedBox(
+                      height: 16, // EXPのゲージを太くします
+                      child: LinearProgressIndicator(
+                        borderRadius: BorderRadius.circular(50),
+                        value: now / achievement,
+                        backgroundColor: Colors.grey[300],
+                        valueColor: const AlwaysStoppedAnimation(
+                          AppColor.primaryColor,
                         ),
                       ),
                     ),
-                    Text(
-                      '${now.toString()} / ${achievement.toString()}',
-                      style: const TextStyle(
-                        fontWeight: FontWeight.bold,
-                        color: AppColor.white,
-                      ),
+                  ),
+                  Text(
+                    '$now / $achievement',
+                    style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: AppColor.white,
                     ),
-                  ],
-                ),
-              ],
-            ),
+                  ),
+                ],
+              ),
+            ],
           ),
         ),
       ),
