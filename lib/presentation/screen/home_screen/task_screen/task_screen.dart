cf4842ce2bb4851db07118a672dc536eaf421402
maple-fm <fuma.deguchi@gmail.com>
2023-10-08 01:56:06 +0900

    remove: task_screenの作成と移動

--- lib/presentation/screen/home_screen/task_screen/task_screen.dart
+++ lib/presentation/screen/home_screen/task_screen/task_screen.dart
@@ -1,19 +1,31 @@
 import 'package:flutter/material.dart';
 
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+import 'package:food_quest/domain/notifier/question_task_notifier.dart';
 import 'package:food_quest/presentation/screen/home_screen/component/task_component.dart';
 
-class TaskScreen extends StatelessWidget {
+class TaskScreen extends HookConsumerWidget {
   const TaskScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final taskList = ref.watch(
+          questionTaskNotifierProvider.select((state) => state.taskList),
+        ) ??
+        [];
+    print('taskList: $taskList');
+
+    if (taskList.isEmpty) {
+      return const SizedBox.shrink();
+    }
     return Scaffold(
       body: ListView.builder(
-        itemCount: 10,
+        itemCount: taskList.length,
         itemBuilder: (context, index) {
+          final task = taskList[index];
           return const Padding(
             padding: EdgeInsets.symmetric(horizontal: 10),
-            // TO-DO: DBから取得したデータを表示する
             child: TaskComponent(
               text: '質問を作成する',
               achievement: 4,
