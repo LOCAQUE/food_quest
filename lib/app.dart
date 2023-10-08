37c2cac1467e079542afd65510a6942107b6254a
maple-fm <fuma.deguchi@gmail.com>
2023-10-09 02:31:45 +0900

    feat: スプラッシュ画面の実装

--- lib/app.dart
+++ lib/app.dart
@@ -1,12 +1,8 @@
 import 'package:flutter/material.dart';
 
-import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:supabase_flutter/supabase_flutter.dart';
 
-import 'package:food_quest/foundation/supabase_client_provider.dart';
-import 'package:food_quest/presentation/screen/bottom_navigation/bottom_navigation_screen.dart';
-import 'package:food_quest/presentation/screen/top.dart';
+import 'package:food_quest/presentation/screen/splash.dart';
 import 'package:food_quest/theme.dart';
 
 class App extends HookConsumerWidget {
@@ -14,33 +10,11 @@ class App extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final auth = ref.watch(supabaseAuthProvider);
-    final isSignIn = useState(false);
-
-    useEffect(
-      () {
-        WidgetsBinding.instance.addPostFrameCallback((_) {
-          auth.onAuthStateChange.listen((event) {
-            if (event.event == AuthChangeEvent.signedIn) {
-              isSignIn.value = true;
-              return;
-            } else {
-              isSignIn.value = false;
-              return;
-            }
-          });
-          return;
-        });
-        return null;
-      },
-      const [],
-    );
-
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'beta',
       theme: customTheme(),
-      home: isSignIn.value ? const BottomNavigationScreen() : const TopScreen(),
+      home: const SplashWidget(),
     );
   }
 }
---
+++ lib/presentation/screen/splash.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+import 'package:food_quest/foundation/supabase_client_provider.dart';
+import 'package:food_quest/presentation/screen/top.dart';
+
+import 'package:food_quest/presentation/screen/bottom_navigation/bottom_navigation_screen.dart'; // おそらくこれが必要です
+
+class SplashWidget extends ConsumerStatefulWidget {
+  const SplashWidget({super.key});
+
+  @override
+  _SplashWidgetState createState() => _SplashWidgetState();
+}
+
+class _SplashWidgetState extends ConsumerState<SplashWidget> {
+  @override
+  void initState() {
+    super.initState();
+    _navigateToHome();
+  }
+
+  _navigateToHome() async {
+    // 例：3秒待機
+    await Future.delayed(const Duration(seconds: 3));
+
+    final auth = ref.watch(supabaseAuthProvider);
+    var isSignIn = false;
+
+    auth.onAuthStateChange.listen((event) {
+      if (event.event == AuthChangeEvent.signedIn) {
+        isSignIn = true;
+      } else {
+        isSignIn = false;
+      }
+    });
+
+    await Navigator.of(context).pushReplacement(
+      MaterialPageRoute(
+        builder: (_) =>
+            isSignIn ? const BottomNavigationScreen() : const TopScreen(),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Image.asset('assets/gif/splash.gif'),
+      ),
+    );
+  }
+}
