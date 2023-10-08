8721e38fc698766110235ba8193aac6e8c158566
kaka0913 <kabuyuu0913@gmail.com>
2023-10-08 14:38:53 +0900

    モデル作成

---
+++ lib/domain/entity/mon_choice_data.g.dart
@@ -0,0 +1,17 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'mon_choice_data.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$MonChoiceDataImpl _$$MonChoiceDataImplFromJson(Map<String, dynamic> json) =>
+    _$MonChoiceDataImpl(
+      baseMonster: json['baseMonster'] as int,
+    );
+
+Map<String, dynamic> _$$MonChoiceDataImplToJson(_$MonChoiceDataImpl instance) =>
+    <String, dynamic>{
+      'baseMonster': instance.baseMonster,
+    };
