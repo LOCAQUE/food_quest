db54ba8c773ec9332b1154be4d7cfeb791b0f869
tenpei-sano <kikichan.fps@gmail.com>
2023-10-08 22:29:23 +0900

    notifier作成

---
+++ lib/domain/notifier/answer_notifier.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/cupertino.dart';
+
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:food_quest/domain/entity/answer.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+import 'package:food_quest/domain/entity/question.dart';
+import 'package:food_quest/foundation/supabase_client_provider.dart';
+
+part 'answer_notifier.freezed.dart';
+
+@freezed
+class AnswerNotifierState with _$AnswerNotifierState {
+  factory AnswerNotifierState({
+    List<QuestionResponse>? questionList,
+  }) = _AnswerNotifierState;
+}
+
+final answerNotifierProvider =
+StateNotifierProvider<AnswerNotifier, AnswerNotifierState>(
+        (ref) {
+      final client = ref.watch(supabaseClientProvider);
+      return AnswerNotifier(client, ref);
+    });
+
+class AnswerNotifier extends StateNotifier<AnswerNotifierState> {
+  AnswerNotifier(this.client, this.ref)
+      : super(AnswerNotifierState());
+  final SupabaseClient client;
+  final Ref ref;
+
+  final TextEditingController contentController = TextEditingController();
+  final TextEditingController minimumBudgetController = TextEditingController();
+  final TextEditingController maximumBudgetController = TextEditingController();
+
+  Future<void> createAnswer({required int questId}) async {
+    final currentUserId = client.auth.currentUser?.id;
+    final sendAnswerData = SendAnswer(
+      content: contentController.text,
+      uid: currentUserId!,
+      minimumBudget: int.parse(minimumBudgetController.text),
+      maximumBudget: int.parse(maximumBudgetController.text),
+      bestAnswer: false,
+      questId: questId,
+    );
+
+    try {
+      await client.from('answers').insert(sendAnswerData);
+    } catch (e) {
+      debugPrint(e.toString());
+    }
+  }
+
+  //ホームの質問一覧
+  Future<void> getQuestList() async {
+    try {
+      final response =
+      await client.from('quests').select<PostgrestList>('*, users(*)');
+
+      final questionList = response.map(QuestionResponse.fromJson).toList();
+      state = state.copyWith(questionList: questionList);
+    } catch (e) {
+      debugPrint(e.toString());
+    }
+  }
+}
