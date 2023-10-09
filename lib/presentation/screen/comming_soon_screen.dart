44ff758bb87b802991cce94bc074ed338090ad24
tenpei-sano <kikichan.fps@gmail.com>
2023-10-09 12:25:59 +0900

    カミングスーンページ

---
+++ lib/presentation/screen/comming_soon_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+class ComingSoonScreen extends HookConsumerWidget {
+  const ComingSoonScreen({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return const Scaffold(
+      body: Center(
+        child: Text('coming soon...'),
+      ),
+    );
+  }
+}
