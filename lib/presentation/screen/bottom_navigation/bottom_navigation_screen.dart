5ad44fa3e4e5a174f4d891b4f88d30e82d434820
maple-fm <fuma.deguchi@gmail.com>
2023-10-09 14:23:20 +0900

    削除

--- lib/presentation/screen/bottom_navigation/bottom_navigation_screen.dart
+++ lib/presentation/screen/bottom_navigation/bottom_navigation_screen.dart
@@ -9,7 +9,6 @@ import 'package:navigator_scope/navigator_scope.dart';
 
 import 'package:food_quest/gen/colors.gen.dart';
 import 'package:food_quest/presentation/screen/home_screen/home_screen.dart';
-import 'package:food_quest/presentation/screen/orange_screen/orange_screen.dart';
 import 'package:food_quest/presentation/screen/pet/pet_screen.dart';
 import 'package:food_quest/presentation/screen/profile_screen/profile_screen.dart';
 import 'package:food_quest/presentation/screen/setting_screen/setting_screen.dart';
