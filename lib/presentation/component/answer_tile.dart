42eb1311e2c2a179883ec37798701361592d6649
tenpei-sano <kikichan.fps@gmail.com>
2023-10-09 13:47:36 +0900

    BTボタン

--- lib/presentation/component/answer_tile.dart
+++ lib/presentation/component/answer_tile.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
-
+import 'package:food_quest/domain/notifier/answer_notifier.dart';
 import 'package:gap/gap.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import 'package:food_quest/domain/entity/answer.dart';
 import 'package:food_quest/domain/entity/user_data.dart';
+
+import 'button.dart';
 
 class AnswerTile extends HookConsumerWidget {
   const AnswerTile({
@@ -21,6 +23,10 @@ class AnswerTile extends HookConsumerWidget {
     BuildContext context,
     WidgetRef ref,
   ) {
+
+    final currentUserId = ref.watch(
+      answerNotifierProvider.select((state) => state.currentUserId),
+    );
     return Card(
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(16),
@@ -31,9 +37,22 @@ class AnswerTile extends HookConsumerWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text(
-              user.name,
-              style: const TextStyle(fontWeight: FontWeight.bold),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  user.name,
+                  style: const TextStyle(fontWeight: FontWeight.bold),
+                ),
+                if(currentUserId == user.id)
+                  CustomButton(
+                    text: 'BA',
+                    variant: ButtonVariant.outline,
+                    onPressed: () async {
+                    },
+                    size: ButtonSize.small,
+                  )
+              ],
             ),
             const Gap(32),
             Text(answer.content),
