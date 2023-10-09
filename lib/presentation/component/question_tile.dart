b395b2dabf0ce400ad09242c8319495566522a6e
kaka0913 <kabuyuu0913@gmail.com>
2023-10-09 14:29:03 +0900

    質問タイルの細かいUI修正

--- lib/presentation/component/question_tile.dart
+++ lib/presentation/component/question_tile.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-
 import 'package:gap/gap.dart';
-
 import 'package:food_quest/domain/entity/question.dart';
 
 class QuestionTile extends StatelessWidget {
@@ -24,22 +22,59 @@ class QuestionTile extends StatelessWidget {
         ),
         color: Colors.white,
         child: Padding(
-          padding: const EdgeInsets.all(16),
+          padding: const EdgeInsets.fromLTRB(16, 16, 16, 16),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Text(
-                question.users?.name ?? '',
-                style: const TextStyle(fontWeight: FontWeight.bold),
-              ),
-              const Gap(32),
-              Text(question.contents),
-              const Gap(8),
               Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Text(question.users?.prefecture ?? ''),
-                  Text(question.formattedDeadLine),
+                  const CircleAvatar(
+                    backgroundColor: Colors.grey, // グレーの背景色を設定
+                    radius: 24, // 丸の半径を調整
+                    child: Icon(
+                      Icons.person, // 代替のアイコンを設定
+                      color: Colors.white, // アイコンの色を白に設定
+                    ),
+                  ),
+                  const Gap(8),
+                  Text(
+                    question.users?.name ?? '',
+                    style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                    ),
+                  ),
+                ],
+              ),
+              const Gap(20),
+              Text(
+                question.contents,
+                style: const TextStyle(
+                  fontSize: 14,
+                ),
+              ),
+              const Gap(20),
+              Row(
+                children: [
+                  const Icon(Icons.place,
+                   color: Colors.black, 
+                   size: 14,), // 地点のアイコン
+                  const Gap(8),
+                  Text(
+                    question.users?.prefecture ?? '',
+                    style: const TextStyle(
+                      fontSize: 12,
+                      color: Colors.black,
+                    ),
+                  ),
+                  const Gap(200),
+                  Text(
+                    question.formattedDeadLine,
+                    style: const TextStyle(
+                      fontSize: 12,
+                      color: Colors.grey,
+                    ),
+                  ),
                 ],
               ),
             ],
