b276ff2b2ed4796b396e72a24a8e24d1e7ec318f
tenpei-sano <kikichan.fps@gmail.com>
2023-11-12 18:16:59 +0900

    shared_prefrencesの導入とproviderの作成

---
+++ lib/domain/repositories/shared_preferences_repository.dart
@@ -0,0 +1,15 @@
+import 'package:food_quest/foundation/shared_preferences_client_provider.dart';
+import 'package:food_quest/infrastructure/local/shared_preferences_repository_impl.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+final sharedPreferencesRepositoryProvider =
+    Provider<SharedPreferencesRepository>((ref) {
+  //clientをDIする
+  final provider = ref.watch(sharedPreferencesClientProvider);
+
+  final client = provider.value;
+  //ここを書き換えると、別の実装に差し替えられる
+  return SharedPreferencesRepositoryImpl(client!);
+});
+
+abstract class SharedPreferencesRepository {}
---
+++ lib/infrastructure/local/shared_preferences_repository_impl.dart
@@ -0,0 +1,10 @@
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:food_quest/domain/repositories/shared_preferences_repository.dart';
+
+//sharedPreferencesの実際の処理を書く
+class SharedPreferencesRepositoryImpl implements SharedPreferencesRepository {
+  SharedPreferencesRepositoryImpl(this.client);
+
+  final SharedPreferences client;
+  
+}
