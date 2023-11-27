a5cea341db0eb501ef6924a16ddeeeeef6987990
tenpei-sano <kikichan.fps@gmail.com>
2023-11-27 22:10:51 +0900

    やること

--- lib/domain/application/notifier/mon_choice_notifier.dart
+++ lib/domain/application/notifier/mon_choice_notifier.dart
@@ -1,28 +1,37 @@
 import 'package:flutter/cupertino.dart';
 import 'package:food_quest/domain/repositories/api_repository.dart';
-import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 import 'package:food_quest/domain/entity/mon_choice_data.dart';
 
-part 'mon_choice_notifier.freezed.dart';
+// part 'mon_choice_notifier.freezed.dart';
 part 'mon_choice_notifier.g.dart';
 
-@freezed
-class MonchoiceNotifierState with _$MonchoiceNotifierState {
-  factory MonchoiceNotifierState({
-    String? currentUserId,
-    MonChoiceData? monChoideData,
-  }) = _MonchoiceNotifierState;
-}
+// @freezed
+// class MonchoiceNotifierState with _$MonchoiceNotifierState {
+//   factory MonchoiceNotifierState({
+//     String? currentUserId,
+//     MonChoiceData? monChoideData,
+//   }) = _MonchoiceNotifierState;
+// } いらない
+
+//フォルダの位置変更
 
 @riverpod
 class MonchoiceNotifier extends _$MonchoiceNotifier {
+  //モンスターのデータを取得する
   @override
-  MonchoiceNotifierState? build() {
-    return null;
+  Future<MonChoiceData?> build() async {
+    try {
+      final repository = ref.read(apiRepositoryProvider);
+      return await repository.getBaseMonster();
+    } catch (e) {
+      debugPrint(e.toString());
+      return null;
+    }
   }
 
+//名前も追加する
   Future<void> addMonster(int selectedPet) 
     async {
     try {
@@ -31,17 +40,5 @@ class MonchoiceNotifier extends _$MonchoiceNotifier {
     } catch (e) {
       debugPrint(e.toString());
     }
-  }
-  
-  Future<MonChoiceData?> getBaseMonster() 
-    async {
-    try {
-      final repository = ref.read(apiRepositoryProvider);
-      await repository.getBaseMonster(
-      );
-    } catch (e) {
-      debugPrint(e.toString());
-    }
-    return null;
   }
 }
