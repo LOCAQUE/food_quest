9deb91efca4371ceebdea13dd397d136bd45fee2
tenpei-sano <kikichan.fps@gmail.com>
2023-12-01 07:39:02 +0900

    現在位置取得

---
+++ lib/domain/application/google_map/geolocator/notifier/geolocator_notifier.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'geolocator_notifier.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$geolocatorNotifierHash() =>
+    r'b478966ac70d6ab6c937335b1d188f8234f24dc8';
+
+/// See also [GeolocatorNotifier].
+@ProviderFor(GeolocatorNotifier)
+final geolocatorNotifierProvider =
+    AutoDisposeAsyncNotifierProvider<GeolocatorNotifier, Position>.internal(
+  GeolocatorNotifier.new,
+  name: r'geolocatorNotifierProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$geolocatorNotifierHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$GeolocatorNotifier = AutoDisposeAsyncNotifier<Position>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
