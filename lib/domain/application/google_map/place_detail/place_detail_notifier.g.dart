5a850d9f680af94bbf9719ec930dce34aca69e12
tenpei-sano <kikichan.fps@gmail.com>
2023-12-01 07:39:33 +0900

    場所詳細取得

---
+++ lib/domain/application/google_map/place_detail/place_detail_notifier.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'place_detail_notifier.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$placeDetailNotifierHash() =>
+    r'e416b9bbf51840a930e041c114952c6f86108e1c';
+
+/// See also [PlaceDetailNotifier].
+@ProviderFor(PlaceDetailNotifier)
+final placeDetailNotifierProvider = AutoDisposeAsyncNotifierProvider<
+    PlaceDetailNotifier, DetailsResponse?>.internal(
+  PlaceDetailNotifier.new,
+  name: r'placeDetailNotifierProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$placeDetailNotifierHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$PlaceDetailNotifier = AutoDisposeAsyncNotifier<DetailsResponse?>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
