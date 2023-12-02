cde3dde8d66e91bbb12638a3866883ac16a7fd91
maple-fm <fuma.deguchi@gmail.com>
2023-12-02 17:18:55 +0900

    update: ボタンの適用

--- lib/presentation/screen/top.dart
+++ lib/presentation/screen/top.dart
@@ -21,22 +21,19 @@ class TopScreen extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            // TO-DO: タイトルの修正
             Image.asset('assets/images/logo.png'),
             const Gap(110),
             CustomButton(
               variant: ButtonVariant.primary,
-              text: '新規登録はこちら',
-              buttonRadius: ButtonRadius.square,
+              text: 'はじめての方はこちら',
               onPressed: () {
                 context.pushRoute(const SignUpRoute());
               },
             ),
-            const Gap(30),
+            const Gap(32),
             CustomButton(
               text: 'ログイン',
-              variant: ButtonVariant.outline,
-              buttonRadius: ButtonRadius.square,
+              variant: ButtonVariant.secondary,
               onPressed: () {
                 Navigator.push(
                   context,
@@ -46,43 +43,6 @@ class TopScreen extends StatelessWidget {
                 );
               },
             ),
-            const Gap(26),
-            SizedBox(
-              width: 327,
-              height: 20,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                    height: 1, // 棒線の高さを設定
-                    color: AppColor.disabledColor, // 棒線の色を設定
-                    width: 118, // 棒線の幅を設定
-                  ),
-                  const Text(
-                    ' OR ',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      color: AppColor.disabledColor,
-                      fontSize: 14,
-                    ),
-                  ),
-                  Container(
-                    height: 1, // 棒線の高さを設定
-                    color: AppColor.disabledColor, // 棒線の色を設定
-                    width: 118, // 棒線の幅を設定
-                  ),
-                ],
-              ),
-            ),
-            const Gap(26),
-            // TO-DO: Googleログインの実装
-            CustomButton(
-              text: 'Googleでログイン',
-              variant: ButtonVariant.outline,
-              buttonRadius: ButtonRadius.square,
-              onPressed: () {},
-            ),
-            const Gap(15),
           ],
         ),
       ),
