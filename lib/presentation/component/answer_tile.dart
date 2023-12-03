adfb43a9c5c8be21f3930867a610b4221cdf16a5
tenpei-sano <kikichan.fps@gmail.com>
2023-12-03 22:09:55 +0900

    ベストアンサーがあればベストアンサーのところに表示させる

--- lib/presentation/component/answer_tile.dart
+++ lib/presentation/component/answer_tile.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:food_quest/domain/application/answer/usecase/grouped_answers.dart';
 import 'package:gap/gap.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
@@ -11,11 +12,70 @@ import 'package:food_quest/foundation/supabase_client_provider.dart';
 import 'package:food_quest/gen/assets.gen.dart';
 import 'package:food_quest/gen/colors.gen.dart';
 
-class AnswerTile extends HookConsumerWidget {
-  const AnswerTile({
+class BestAnswerTile extends HookConsumerWidget {
+  const BestAnswerTile({
+    required this.answer,
+    required this.user,
+    required this.isBestAnswer,
+    super.key,
+  });
+
+  final Answer answer;
+  final UserData user;
+  final bool isBestAnswer;
+
+  @override
+  Widget build(
+    BuildContext context,
+    WidgetRef ref,
+  ) {
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(16),
+      ),
+      color: Colors.white,
+      child: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              children: [
+                Text(
+                  user.name,
+                  style: const TextStyle(fontWeight: FontWeight.bold),
+                ),
+                if (isBestAnswer)
+                  SizedBox(
+                    width: 72,
+                    height: 72,
+                    child: Assets.images.test.image(),
+                  ),
+              ],
+            ),
+            const Gap(32),
+            Text(answer.content),
+            const Gap(8),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(user.name),
+                Text(answer.formattedCreatedAt),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class NotBestAnswerTile extends HookConsumerWidget {
+  const NotBestAnswerTile({
     required this.answer,
     required this.user,
     required this.isMyQuest,
+    required this.questId,
     required this.isContainBestAnswer,
     super.key,
   });
@@ -23,6 +83,7 @@ class AnswerTile extends HookConsumerWidget {
   final Answer answer;
   final UserData user;
   final bool isMyQuest;
+  final int questId;
   final bool isContainBestAnswer;
 
   @override
@@ -30,7 +91,7 @@ class AnswerTile extends HookConsumerWidget {
     BuildContext context,
     WidgetRef ref,
   ) {
-    final isBestAnswer = useState<bool>(answer.bestAnswer);
+    final isBestAnswerState = useState<bool>(answer.bestAnswer);
     final isMyAnswer = answer.uid == ref.read(supabaseCurrentUser)!.id;
 
     return Card(
@@ -60,11 +121,11 @@ class AnswerTile extends HookConsumerWidget {
                           .updateBestAnswer(
                             answerId: answer.id,
                           );
-                      // アンサーを再取得
-                      await ref
-                          .read(answerNotiierProvider.notifier)
-                          .getAnswerList(questId: answer.questId);
-                      isBestAnswer.value = true;
+                      // ベストアンサーの状態を更新
+                      ref.refresh(
+                        GroupedAnswersNotfiierProvider(questId: questId),
+                      );
+                      isBestAnswerState.value = true;
                     },
                     style: ElevatedButton.styleFrom(
                       backgroundColor: AppColor.primaryColor,
@@ -77,12 +138,6 @@ class AnswerTile extends HookConsumerWidget {
                       'ベストアンサー',
                       style: TextStyle(fontSize: 12),
                     ),
-                  ),
-                if (isBestAnswer.value)
-                  SizedBox(
-                    width: 72,
-                    height: 72,
-                    child: Assets.images.test.image(),
                   ),
               ],
             ),
--- lib/presentation/screen/quest_screen/quest_list_screen/quest_detail_screen/quest_detail_screen.dart
+++ lib/presentation/screen/quest_screen/quest_list_screen/quest_detail_screen/quest_detail_screen.dart
@@ -2,10 +2,10 @@ import 'package:flutter/material.dart';
 
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:food_quest/domain/application/answer/usecase/grouped_answers.dart';
 import 'package:gap/gap.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-import 'package:food_quest/domain/application/answer/notifier/answer_notifier.dart';
 import 'package:food_quest/domain/entity/answer.dart';
 import 'package:food_quest/domain/entity/question.dart';
 import 'package:food_quest/foundation/supabase_client_provider.dart';
@@ -24,26 +24,20 @@ class QuestDetailScreen extends HookConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final isLoading = useState<bool>(false);
-    final answers = useState<List<Answer>?>([]);
-    final answerNotifier = ref.watch(answerNotiierProvider);
+    final bestAnswer = useState<List<Answer>?>([]);
+    final notBestAnswers = useState<List<Answer>?>([]);
+    final groupedAnswersNotifier =
+        ref.watch(GroupedAnswersNotfiierProvider(questId: question.id));
     final currentUserId = ref.read(supabaseCurrentUser)!.id;
     final isMyQuest = question.isMyQuest(currentUserId: currentUserId);
+    //ベストアンサーがあるかどうか
+    final isContainBestAnswer = useState<bool>(false);
 
-    useEffect(
-      () {
-        WidgetsBinding.instance.addPostFrameCallback((_) {
-          ref
-              .read(answerNotiierProvider.notifier)
-              .getAnswerList(questId: question.id);
-        });
-        return null;
-      },
-      const [],
-    );
-
-    answerNotifier.when(
+    groupedAnswersNotifier.when(
       data: (value) {
-        answers.value = value;
+        bestAnswer.value = value!['isBestAnswer'];
+        notBestAnswers.value = value['notBestAnswer'];
+        isContainBestAnswer.value = value['isBestAnswer']!.isNotEmpty;
         isLoading.value = false;
       },
       loading: () {
@@ -83,35 +77,31 @@ class QuestDetailScreen extends HookConsumerWidget {
                   onTap: () {},
                 ),
               ),
-              const Gap(8),
-              const Align(
-                alignment: Alignment.centerLeft,
-                child: Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 16),
-                  child: Text(
-                    '回答',
-                    style: TextStyle(fontWeight: FontWeight.bold),
-                  ),
-                ),
-              ),
-              const Gap(8),
-              if (answers.value!.isEmpty) const Center(child: Text('回答がありません')),
+              const Gap(20),
               if (isLoading.value) const LoadingWidget(),
+              if (bestAnswer.value!.isEmpty && notBestAnswers.value!.isEmpty)
+                const Column(
+                  children: [
+                    Gap(16),
+                    Center(child: Text('回答がありません')),
+                  ],
+                ),
 
-              //回答
-              if (answers.value!.isNotEmpty)
-                ...List.generate(answers.value!.length, (index) {
-                  //回答のベストアンサーがあるかどうか
-                  final isContainBestAnswer =
-                      answers.value!.any((answer) => answer.bestAnswer);
+              //ベストアンサー
+              if (bestAnswer.value!.isNotEmpty)
+                BestAnswerWidget(
+                  bestAnswer: bestAnswer,
+                  question: question,
+                ),
 
-                  return AnswerTile(
-                    user: question.users!,
-                    answer: answers.value![index],
-                    isMyQuest: isMyQuest,
-                    isContainBestAnswer: isContainBestAnswer,
-                  );
-                }),
+              //その他の回答
+              if (notBestAnswers.value!.isNotEmpty)
+                NotBestAnswers(
+                  notBestAnswers: notBestAnswers,
+                  question: question,
+                  isMyQuest: isMyQuest,
+                  isContainBestAnswer: isContainBestAnswer.value,
+                ),
             ],
           ),
         ],
@@ -127,6 +117,85 @@ class QuestDetailScreen extends HookConsumerWidget {
         backgroundColor: AppColor.primaryColor,
         child: const Icon(Icons.add),
       ),
+    );
+  }
+}
+
+class BestAnswerWidget extends StatelessWidget {
+  const BestAnswerWidget({
+    required this.bestAnswer,
+    required this.question,
+    super.key,
+  });
+
+  final ValueNotifier<List<Answer>?> bestAnswer;
+  final QuestionResponse question;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        const Align(
+          alignment: Alignment.centerLeft,
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 16),
+            child: Text(
+              'ベストアンサー',
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+          ),
+        ),
+        ...List.generate(bestAnswer.value!.length, (index) {
+          return BestAnswerTile(
+            answer: bestAnswer.value![index],
+            user: question.users!,
+            isBestAnswer: true,
+          );
+        }),
+        const Gap(20),
+      ],
+    );
+  }
+}
+
+class NotBestAnswers extends StatelessWidget {
+  const NotBestAnswers({
+    required this.notBestAnswers,
+    required this.question,
+    required this.isMyQuest,
+    required this.isContainBestAnswer,
+    super.key,
+  });
+
+  final ValueNotifier<List<Answer>?> notBestAnswers;
+  final QuestionResponse question;
+  final bool isMyQuest;
+  final bool isContainBestAnswer;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        const Align(
+          alignment: Alignment.centerLeft,
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 16),
+            child: Text(
+              'その他の回答',
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+          ),
+        ),
+        ...List.generate(notBestAnswers.value!.length, (index) {
+          return NotBestAnswerTile(
+            answer: notBestAnswers.value![index],
+            user: question.users!,
+            isMyQuest: isMyQuest,
+            questId: question.id,
+            isContainBestAnswer: isContainBestAnswer,
+          );
+        })
+      ],
     );
   }
 }
